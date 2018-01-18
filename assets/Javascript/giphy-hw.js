eac1137eb61663992fd855c736a91155480a64da
Eric <ericsteinman@me.com>
2018-01-18 14:10:17 -0600

    Added some basic HTML and start the JS

---
+++ assets/Javascript/giphy-hw.js
@@ -0,0 +1,5 @@
+$( document ).ready(function() {
+
+// Created an array for the 'topics'. I chose Pokemon. Any new 'topics' added will be pushed here.
+var topics = ["Pikachu", "Jigglypuff", "Charmander", "Lugia", "Umbreon", "Arcanine", "Togepei", "Squirtle", "Ninetails", "Mewtwo","Gyarados", "Charizard", "Cubone", "Arbok", "Torracat"];
+
